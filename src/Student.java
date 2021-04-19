178d851ff63145b3144076243d4673f16ffb804d
VoHoangDuong <duong6046237@gmail.com>
2021-04-19 15:28:34 +0700

    update

---
+++ src/Student.java
@@ -0,0 +1,44 @@
+public class Student {
+    private String name;
+    private int age;
+    private String address;
+
+    public Student(String name, int age, String address){
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public Student(){
+
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "name= " + name + '\'' + ", age=" + age + ", address='" + address + '\'';
+    }
+}
